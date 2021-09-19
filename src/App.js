a0d088079a48da3d713e458b88810c16d40ac6b2
Trevor Womack Jr <Twomack6823@gmail.com>
2021-09-18 23:23:22 -0700

    Following along video, lots of errors though

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
-import { Physics, useBox, usePlane } from '@react-three/cannon';
-import './App.css';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import { Ground } from './Components/Ground';
+import { Player } from './Components/Player';
+import { Cube } from './Components/Cube';
 
-function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-  return (
-    <mesh onClick={() => {
-      api.velocity.set(0, 2, 0)}}
-    ref={ref} position={[0,2,0]} >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  )
-}
-
-function Plane() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0]
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  )
-}
 function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Physics>
-        <Box />
-        <Plane />
-      </Physics>
-    </Canvas>
+      <Canvas shadowMap sRGB>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={0.25} />
+        <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
+        <Physics gravity={[0, -30, 0]}>
+          <Ground position={[0, 0.5, 0]}/>
+          <Player position={[0, 3, 10]}/>
+          <Cube position={[0, 1, 0]} type="wood" />
+        </Physics>
+      </Canvas>
   );
 }
 
---
+++ src/Textures.js
@@ -0,0 +1,23 @@
+import dirtImg from './Images/dirt.jpg';
+import grassImg from './Images/grass.jpg';
+import glassImg from './Images/glass.png';
+import logImg from './Images/log.jpg';
+import woodImg from './Images/wood.png';
+import { TextureLoader, NearestFilter, LinearMipMapLinearFilter  } from 'three';
+
+export const dirt = new TextureLoader().load(dirtImg);
+export const grass = new TextureLoader().load(grassImg);
+export const glass = new TextureLoader().load(glassImg);
+export const wood = new TextureLoader().load(woodImg);
+export const log = new TextureLoader().load(logImg);
+
+dirt.magFilter = NearestFilter;
+dirt.minFilter = LinearMipMapLinearFilter;
+grass.magFilter = NearestFilter;
+grass.minFilter = LinearMipMapLinearFilter;
+glass.magFilter = NearestFilter;
+glass.minFilter = LinearMipMapLinearFilter;
+wood.magFilter = NearestFilter;
+wood.minFilter = LinearMipMapLinearFilter;
+log.magFilter = NearestFilter;
+log.minFilter = LinearMipMapLinearFilter
